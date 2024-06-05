57659fe6cf3051c878d7864fe1ac84ac91c486d9
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-06-05 10:43:39 -0700

    Complete first setup steps

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+
+// to turn on node watch: 
+// node --watch main.js
+// control + c to cancel
+
+// To log in terminal: 
+// node -e "console.log(123)" 
+
+
+console.log(`Sup dude`)
+console.log(`other things`)
