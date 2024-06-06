17fe669bf3ffbc380f38d45a10d40377dc1b59e2
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-06-06 10:03:24 -0700

    Add write file with node example

--- main.js
+++ main.js
@@ -1,12 +1,22 @@
-#!/usr/bin/node
-
-// to turn on node watch: 
-// node --watch main.js
-// control + c to cancel
-
-// To log in terminal: 
-// node -e "console.log(123)" 
+const fs = require('node:fs');
 
 
-console.log(`Sup dude`)
-console.log(`other things`)
+
+// How to write a file: 
+const content = 'some content!';
+fs.writeFile('./test.txt', content, err => {
+    if (err) {
+        console.error(err);
+    } else {
+        console.log(`hooray!`);
+    }
+})
+
+//  Writing a file synchronously:
+const content2 = 'some more content!';
+try {
+    fs.writeFileSync('/Users/joe/test.txt', content);
+    // file written successfully
+  } catch (err) {
+    console.error(err);
+  }
