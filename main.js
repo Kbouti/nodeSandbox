8f398ad585c5585bc60734e374c59df3cd6b9746
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-06-06 10:27:42 -0700

    Add event Emitter example

--- main.js
+++ main.js
@@ -1,58 +1,9 @@
-const fs = require("node:fs");
+const EventEmitter = require('node:events');
+const eventEmitter = new EventEmitter();
 
-// How to write a file:
-const content = "some content!";
-fs.writeFile("./test.txt", content, (err) => {
-  if (err) {
-    console.error(err);
-  } else {
-    console.log(`hooray!`);
-  }
-});
-
-//  Writing a file synchronously:
-const content2 = "some more content!";
-try {
-  fs.writeFileSync("/Users/joe/test.txt", content);
-  // file written successfully
-} catch (err) {
-  console.error(err);
-}
+eventEmitter.on('start', () => {
+    console.log('started');
+  });
 
 
-
-// Reading files with Node: 
-
-const fs = require('node:fs');
-
-fs.readFile('/Users/joe/test.txt', 'utf8', (err, data) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  console.log(data);
-});
-
-// And the syncronus version: 
-const fs = require('node:fs');
-
-try {
-  const data = fs.readFileSync('/Users/joe/test.txt', 'utf8');
-  console.log(data);
-} catch (err) {
-  console.error(err);
-}
-
-
-// And the promise based: 
-const fs = require('node:fs/promises');
-
-async function example() {
-  try {
-    const data = await fs.readFile('/Users/joe/test.txt', { encoding: 'utf8' });
-    console.log(data);
-  } catch (err) {
-    console.log(err);
-  }
-}
-example();
+  eventEmitter.emit('start');
